bd75fb4acb0e6d5fb40aac01f9aaa03b0293c5c2
Juan Zimmermann <juanzimmermann05@gmail.com>
2023-05-25 18:14:34 -0300

    alterado porta do app

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -134,4 +134,5 @@ def index():
         return '<h1> Rossmann Telegram BOT </h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
